276711a9f7176c35e5420a7a8b914170bba16d91
Moonwaker <richardgavia@gmail.com>
2019-12-11 14:23:58 -0800

    Add files via upload

--- src/def/Account.java
+++ src/def/Account.java
@@ -1,13 +1,18 @@
 package def;
 
+import java.util.List;
+
 public class Account {
 	private String username;
 	
 	private String password;
 	
+	private List<String> dresses;
+	
 	public Account(String username, String password) {
 		this.username = username;
 		this.password = password;
+		dresses = null;
 	}
 	
 	public String getUsername() {
@@ -16,5 +21,9 @@ public class Account {
 	
 	public String getPassword() {
 		return password;
+	}
+	
+	public void updateDresses(String string) {
+		dresses.add(string);
 	}
 }
--- src/def/Main.java
+++ src/def/Main.java
@@ -1,8 +1,10 @@
 package def;
 
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -12,22 +14,33 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import com.mongodb.MongoClient;
 import com.mongodb.diagnostics.logging.Logger;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSInputFile;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 
@@ -38,13 +51,13 @@ public class Main extends Application {
 
     private VBox vBox1, vBox2;
     
-    private HBox hBox5;
+    private HBox hBox4, hBox5, hBox8;
 
     private Label userLabel, passLabel, newUser, newPass, heightLabel, shoulderLabel, hipLabel, errorLabel;
     
     private Slider heightSlider, shoulderSlider, hipSlider;
 
-    private Button enterButton, enter1Button, cancelButton, newAccount, logoutButton, bmiButton, dressButton, addDressButton, profileButton;
+    private Button display, choose, enterButton, enter1Button, cancelButton, newAccount, logoutButton, bmiButton, dressButton, addDressButton, profileButton;
 
     private TextField userText, newUserText;
 
@@ -54,6 +67,8 @@ public class Main extends Application {
 
     private BorderPane borderpane;
     
+    private ChoiceBox<String> cb, cb1;
+    
     private MongoClient mongoClient;
     
     private DB database;
@@ -61,6 +76,18 @@ public class Main extends Application {
     private DBCollection collection;
     
     private Account account;
+    
+    private FileInputStream inputStream;
+    
+    private List<String> dressList;
+    
+    private String imagePath;
+    
+    private Image image, image1, newImage;
+    
+    private ImageView imageView, imageView1;
+    
+    private static final int TOLERANCE_THRESHOLD = 0xCF;
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
@@ -76,15 +103,21 @@ public class Main extends Application {
     	database = mongoClient.getDB("accountdatabase");
     	
     	collection = database.getCollection("accounts");
-    	
+
         // Create Controls
-    	FileInputStream inputStream = new FileInputStream("src/figure_silhouette_png.png");
-    	Image image = new Image(inputStream);
-    	ImageView imageView = new ImageView(image);
+    	inputStream = new FileInputStream("src/figure_silhouette_png.png");
+    	image = new Image(inputStream);
+    	imageView = new ImageView(image);
     	imageView.setFitHeight(200);
-    	imageView.setFitWidth(53.5);
+    	imageView.setFitWidth(54);
     	
     	profileButton = new Button("Generate Profile");
+    	
+    	logoutButton = new Button ("Logout");
+        bmiButton = new Button("Input BMI");
+        dressButton = new Button("Choose Dress");
+        addDressButton = new Button("Add Dress");
+        addDressButton.setDisable(false);
     	
     	heightLabel = new Label("Height: ");
     	shoulderLabel = new Label("Shoulder: ");
@@ -139,7 +172,179 @@ public class Main extends Application {
         enterButton = new Button("Enter");
         enterButton.setDisable(true);
         
+        choose = new Button("Choose");
+        display = new Button("Display");
+        
+        cb = new ChoiceBox<>();
+        cb.getItems().addAll("User", "Admin" );
+        cb.getSelectionModel().selectFirst();
+        cb.setDisable(false);
+        
+        cb1 = new ChoiceBox<>();
+        
+        dressList = new ArrayList<String>();
+        
         // Set events to Control
+        dressButton.setOnAction(event -> {
+        	DBObject query = new BasicDBObject("username", userText.getText());
+        	DBCursor cursor = collection.find(query);
+        	DBObject user = cursor.one();
+        	
+        	BasicDBList dresses = (BasicDBList) user.get("dresses");
+        	//List<String> dressList = new ArrayList<String>();
+        	
+        	for (Object dress : dresses) {
+        		dressList.add((String) dress);
+        	}
+        	
+        	for (int i = 0; i < dressList.size(); i++) {
+        		cb1.getItems().add(dressList.get(i));
+        	}       	
+        	  	
+        	HBox hBox7 = new HBox(15, display, choose);
+        	hBox7.setAlignment(Pos.CENTER);
+        	
+        	hBox8 = new HBox();
+        	hBox8.setAlignment(Pos.CENTER);
+        	
+        	HBox hBox9 = new HBox(cb1);
+        	hBox9.setAlignment(Pos.CENTER);
+        	
+        	VBox vBox4 = new VBox(10, hBox9, hBox8, hBox7);
+        	vBox4.setAlignment(Pos.CENTER);
+        	
+        	borderpane.setCenter(vBox4);     	
+        });
+        
+        choose.setOnAction(event -> {
+        	//imageView1.setFitHeight(175);
+        	//imageView1.setFitWidth(53.5);
+        	
+        	
+        	int width = (int) image.getWidth();
+        	int height = (int) image.getHeight();
+        	
+        	Image newNewImage = new Image(new File(cb1.getSelectionModel().getSelectedItem()).toURI().toString(), width, height - 100, false, false);
+        	
+        	int width1 = (int) newNewImage.getWidth();
+        	int height1 = (int) newNewImage.getHeight();
+        	
+        	WritableImage wImage = new WritableImage(width, height);
+        	PixelReader pixelReader = image.getPixelReader();
+        	PixelWriter writer = wImage.getPixelWriter();
+        	
+        	PixelReader pixelReader1 = newNewImage.getPixelReader();
+        	
+        	for(int y = 0; y < height; y++) { 
+                for(int x = 0; x < width; x++) { 
+                   //Retrieving the color of the pixel of the loaded image   
+                   Color color = pixelReader.getColor(x, y); 
+                     
+                   //Setting the color to the writable image 
+                   writer.setColor(x, y, color.darker());              
+                }
+            }
+        	
+        	for(int y = 0; y < height1; y++) { 
+                for(int x = 0; x < width1; x++) { 
+                   //Retrieving the color of the pixel of the loaded image   
+                   Color color = pixelReader1.getColor(x, y); 
+                     
+                   //Setting the color to the writable image 
+                   writer.setColor(x, y, color.darker());              
+                }
+            }
+        	
+        	ImageView newImageView = new ImageView(wImage);
+        	newImageView.setFitHeight(200);
+        	newImageView.setFitWidth(53.5);
+        	
+        	//imageView1.setBlendMode(BlendMode.DIFFERENCE);
+        	
+        	//Group blend = new Group(imageView, imageView1);
+        	
+        	hBox4.getChildren().clear();
+        	
+        	hBox4.getChildren().addAll(newImageView);
+        	
+        	borderpane.setCenter(vBox2);
+        });
+        
+        display.setOnAction(event -> {
+        	try {
+        		File imageFile = new File(cb1.getSelectionModel().getSelectedItem());
+				inputStream = new FileInputStream(imageFile);
+				
+            } catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+            }
+        	
+            image1 = new Image(inputStream);
+            newImage = makeTransparent(image1);
+            imageView1 = new ImageView(newImage);
+            //imageView.setFitHeight(200);
+            //imageView.setFitWidth(53.5);
+            
+            hBox8.getChildren().clear();
+            hBox8.getChildren().add(imageView1);
+        });
+        
+        /*cb1.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() { 
+        	  
+            // if the item of the list is changed 
+            public void changed(ObservableValue ov, Number value, Number new_value) 
+            { 
+            	// !null.equals(dressList.get(new_value.intValue())
+                // set the text for the label to the selected item 
+                if (!null.equals(dressList.get(new_value.intValue()))) {
+                	try {
+        				inputStream = new FileInputStream(cb1.getSelectionModel().getSelectedItem());
+        				
+                    } catch (FileNotFoundException e) {
+        				// TODO Auto-generated catch block
+        				e.printStackTrace();
+                    }
+                	
+                    Image image1 = new Image(inputStream);
+                    ImageView imageView1 = new ImageView(image);
+                    imageView.setFitHeight(200);
+                    imageView.setFitWidth(53.5);
+                    	
+                    gridPane3.addRow(1, imageView1);
+                }
+            } 
+        }); */
+        
+        addDressButton.setOnAction(event -> {
+        	FileChooser fileChooser = new FileChooser();
+        	FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png");
+        	fileChooser.getExtensionFilters().addAll(imageFilter);
+        	File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        	
+        	File imageFile = selectedFile;
+        	GridFS gfsImage = new GridFS(database, "dresses");
+        	
+        	try {
+				GridFSInputFile gfsFile = gfsImage.createFile(imageFile);
+				gfsFile.setFilename(imageFile.toURI().toString());	
+				
+				imagePath = imageFile.getPath();
+				
+				DBObject findQuery = new BasicDBObject("username", userText.getText());
+				//DBObject listItem = new BasicDBObject("dresses", imageFile.toURI().toURL().toString());
+				DBObject listItem = new BasicDBObject("dresses", imagePath);
+				DBObject updateQuery = new BasicDBObject("$push", listItem);
+				
+				collection.update(findQuery, updateQuery);
+
+				gfsFile.save();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+        
         newAccount.setOnAction(event -> {
         	GridPane gridPane2 = new GridPane();
             gridPane2.addRow(0, newUser, newUserText);
@@ -183,12 +388,15 @@ public class Main extends Application {
         	
         	if (cursor.itcount() == 1) {
         		if (user.get("password").equals(passText.getText())) {
-        		
-        		
         			errorLabel.setText("");
+        			
+        			if(cb.getSelectionModel().getSelectedItem().equalsIgnoreCase("User")){
+                        addDressButton.setDisable(true);
+        			}
         		
         			borderpane.setCenter(vBox2);
         			enterButton.setDisable(true);
+        			cb.setDisable(true);
         		}
         		else
         			errorLabel.setText("Password does not match Username");
@@ -200,10 +408,13 @@ public class Main extends Application {
 
         // Create Layout Containers
         buildFileMenu(primaryStage);
-
+        
+        Menu cbMenu = new Menu();
+        cbMenu.setGraphic(cb);
+ 
         borderpane = new BorderPane();
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu, cbMenu);
 
         GridPane gridpane = new GridPane();
         gridpane.addRow(0,userLabel,userText);
@@ -231,16 +442,12 @@ public class Main extends Application {
         // Show the stage
         primaryStage.show();
 
-        // Create Controls
-        logoutButton = new Button ("Logout");
-        bmiButton = new Button("Input BMI");
-        dressButton = new Button("Choose Dress");
-        addDressButton = new Button("Add Dress");
-
         // Set events to Controls
         logoutButton.setOnAction(event -> {
             userText.setText("");
             passText.setText("");
+            addDressButton.setDisable(false);
+            cb.setDisable(false);
             borderpane.setCenter(vBox1);
         });
 
@@ -275,7 +482,7 @@ public class Main extends Application {
         // Create Layout Containers
         HBox hBox3 = new HBox(15, bmiButton, dressButton, addDressButton, logoutButton);
         hBox3.setAlignment(Pos.CENTER);
-        HBox hBox4 = new HBox(imageView);
+        hBox4 = new HBox(imageView);
         hBox4.setAlignment(Pos.CENTER);
 
         vBox2 = new VBox(50, hBox3, hBox4);
@@ -381,5 +588,32 @@ public class Main extends Application {
             borderpane.setCenter(vBox1);
         });
         fileMenu.getItems().addAll(newItem, exitItem);
+    }
+    
+    private Image makeTransparent(Image inputImage) {
+        int W = (int) inputImage.getWidth();
+        int H = (int) inputImage.getHeight();
+        WritableImage outputImage = new WritableImage(W, H);
+        PixelReader reader = inputImage.getPixelReader();
+        PixelWriter writer = outputImage.getPixelWriter();
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                int argb = reader.getArgb(x, y);
+
+                int r = (argb >> 16) & 0xFF;
+                int g = (argb >> 8) & 0xFF;
+                int b = argb & 0xFF;
+
+                if (r >= TOLERANCE_THRESHOLD 
+                        && g >= TOLERANCE_THRESHOLD 
+                        && b >= TOLERANCE_THRESHOLD) {
+                    argb &= 0x00FFFFFF;
+                }
+
+                writer.setArgb(x, y, argb);
+            }
+        }
+
+        return outputImage;
     }
 }
